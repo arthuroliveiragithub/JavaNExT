3cd684b9f7d33505d7e248303a7a0983fc5404de
Arthur Araújo <arthurararaujo@gmail.com>
2021-09-01 19:32:40 -0300

    meu primeiro commit

---
+++ src/JavaEx2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class JavaEx2 {
+
+    public static void main(String[] args) {
+        Scanner leTeclado = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número real: ");
+        double real1 = leTeclado.nextDouble();
+
+        System.out.println("Digite o segundo número real: ");
+        double real2 = leTeclado.nextDouble();
+
+        double soma = real1 + real2;
+        double media = soma/2;
+
+        System.out.println("A média desses números é igual a " + media);
+    }
+}
